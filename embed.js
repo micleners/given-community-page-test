b06206a0b9fa14a57d0b411debce3f839ac9318e
Michael Leners <micleners@gmail.com>
2020-10-03 19:41:03 -0500

    add all fields

--- embed.js
+++ embed.js
@@ -13,36 +13,48 @@ request.onload = function () {
   // Begin accessing JSON data here
   var data = JSON.parse(this.response);
 
-  //
-  //
-  //
-  //
-  //
-  //
-  //
   if (request.status >= 200 && request.status < 400) {
+    console.log(data)
     data.forEach((company) => {
       // Create a div with a "Sample Card" class
       const card = document.createElement('div');
       console.log(card);
-      card.setAttribute('class', 'sample-card');
+      card.setAttribute('class', 'company-card');
 
       // Create an h3 and set the text content to the film's title
       const h3 = document.createElement('h3');
       h3.textContent = company.slug;
-
-      // Create a p and set the text content to the film's description
-      const p = document.createElement('p');
-      p.textContent = company.name;
-      p.textContent = company.displayName;
-      p.textContent = company.logo;
-      p.textContent = company.level;
-      p.textContent = company.website;
-      p.textContent = company.metro;
-
-      // Append h3 and p to the card element
       card.appendChild(h3);
-      card.appendChild(p);
+
+      const pName = document.createElement('p');
+      pName.textContent = company.name;
+      pName.setAttribute('class', 'name-field')
+      card.appendChild(pName);
+
+      const pDisplayName = document.createElement('p');
+      pDisplayName.textContent = company.displayName;
+      pName.setAttribute('class', 'name-field')
+      card.appendChild(pDisplayName);
+
+      const logo = document.createElement('img')
+      logo.src = company.logo
+      logo.alt = "Company Logo"
+      card.appendChild(logo);
+
+      const pLevel = document.createElement('p');
+      pLevel.textContent = company.level;
+      pName.setAttribute('class', 'name-field')
+      card.appendChild(pLevel);
+
+      const pWebsites = document.createElement('p');
+      pWebsites.textContent = company.website;
+      pName.setAttribute('class', 'name-field')
+      card.appendChild(pWebsites);
+
+      const pMetro = document.createElement('p');
+      pMetro.textContent = company.metro;
+      pName.setAttribute('class', 'name-field')
+      card.appendChild(pMetro);
 
       // Append the card to the div with "Cards-Container" id
       document.getElementById('Cards-Container').appendChild(card);
